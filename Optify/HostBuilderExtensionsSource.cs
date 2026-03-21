f2ed18c17680db76f6723486a1bf491d1fa225ae
Jake Carpenter <jake@jakecarpenter.dev>
2026-03-20 22:41:50 -0600

    perf: stop using ISymbol in the provider transformer
    Issue: #6

--- Optify/HostBuilderExtensionsSource.cs
+++ Optify/HostBuilderExtensionsSource.cs
@@ -1,6 +1,5 @@
 using System.Collections.Immutable;
 using System.Text;
-using Microsoft.CodeAnalysis;
 
 namespace Optify;
 
@@ -8,8 +7,15 @@ public static class HostBuilderExtensionsSource
 {
     public const string Filename = "OptifyRegistration.g.cs";
 
-    private static string UseOptifyGenericExtensionSource =
+    private const string UseOptifyGenericExtensionSource =
         """
+            /// <summary>
+            /// Register a single type options type using the conventional name or one specified on the
+            /// <see cref="OptifyAttribute"/> attribute. Using this method will require reflection during runtime.
+            /// </summary>
+            /// <param name="hostBuilder">The <see cref="IHostBuilder"/> instance to extend.</param>
+            /// <typeparam name="T">The type to map configured options to.</typeparam>
+            /// <returns>The extended <see cref="IHostBuilder"/> instance.</returns>
             public static IHostBuilder UseOptify<T>(this IHostBuilder hostBuilder) where T : class, new()
             {
                 hostBuilder.ConfigureServices((ctx, services) =>
@@ -18,11 +24,11 @@ public static class HostBuilderExtensionsSource
                     var attribute = type
                         .GetCustomAttributes(typeof(OptifyAttribute), false)
                         .FirstOrDefault(attr => attr is OptifyAttribute);
-
-                    var sectionName = attribute is OptifyAttribute {SectionName.Length: > 0} attr
+            
+                    var sectionName = attribute is OptifyAttribute { SectionName.Length: > 0 } attr
                         ? attr.SectionName
                         : type.Name;
-
+            
                     services
                         .AddOptions<T>()
                         .Bind(ctx.Configuration.GetSection(sectionName));
@@ -31,7 +37,7 @@ public static class HostBuilderExtensionsSource
             }
         """;
 
-    public static string GenerateSource(ImmutableArray<INamedTypeSymbol> types)
+    public static string GenerateSource(ImmutableArray<OptionsTypeToRegister> optionsToRegister)
     {
         var sb = new StringBuilder();
         sb.AppendLine(
@@ -42,36 +48,45 @@ public static class HostBuilderExtensionsSource
             using Microsoft.Extensions.Hosting;
 
             namespace Optify;
+
+            public static partial class OptifyRegistration
+            {
+                /// <summary>
+                /// Register all configurable options types in the current assembly marked with the
+                /// <see cref="OptifyAttribute"/> attribute.
+                /// </summary>
+                /// <param name="hostBuilder">The <see cref="IHostBuilder"/> instance to extend.</param>
+                /// <returns>The extended <see cref="IHostBuilder"/> instance.</returns>
+                public static IHostBuilder UseOptify(this IHostBuilder hostBuilder)
+                {
+                    hostBuilder.ConfigureServices((ctx, services) =>
+                    {
             """);
-
-        sb.AppendLine();
-        sb.AppendLine("public static partial class OptifyRegistration");
-        sb.AppendLine("{");
+        sb.AppendLine(GenerateRegistrationStatements(optionsToRegister));
         sb.AppendLine(
-            "    public static IHostBuilder UseOptify(this IHostBuilder hostBuilder)");
-        sb.AppendLine("    {");
-        sb.AppendLine("        hostBuilder.ConfigureServices((ctx, services) =>");
-        sb.AppendLine("        {");
-
-        foreach (var type in types)
-        {
-            var fullName = type.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat).Replace("global::", "");
-            var attribute = type.GetAttributes()
-                .FirstOrDefault(x => x?.AttributeClass?.Name == OptifyAttributeSource.ClassName);
-            var sectionNameArg = attribute?.NamedArguments
-                .FirstOrDefault(x => x.Key == OptifyAttributeSource.SectionNamePropertyName);
-            var sectionName = sectionNameArg?.Value.Value as string ?? type.Name;
-            sb.AppendLine("            services");
-            sb.AppendLine($"               .AddOptions<{fullName}>()");
-            sb.AppendLine($"               .Bind(ctx.Configuration.GetSection(\"{sectionName}\"));");
-        }
-
-        sb.AppendLine("        });");
-        sb.AppendLine("        return hostBuilder;");
-        sb.AppendLine("    }");
+            """
+                    });
+                
+                    return hostBuilder;
+                }
+            """);
         sb.AppendLine();
         sb.AppendLine(UseOptifyGenericExtensionSource);
         sb.AppendLine("}");
+
+        return sb.ToString();
+    }
+
+    private static string GenerateRegistrationStatements(ImmutableArray<OptionsTypeToRegister> optionsToRegister)
+    {
+        var sb = new StringBuilder();
+
+        foreach (var optionsType in optionsToRegister)
+        {
+            sb.AppendLine("            services");
+            sb.AppendLine($"               .AddOptions<{optionsType.FullName}>()");
+            sb.AppendLine($"               .Bind(ctx.Configuration.GetSection(\"{optionsType.SectionName}\"));");
+        }
 
         return sb.ToString();
     }
