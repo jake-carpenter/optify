7b0e570f5198a4a69e065c6d671052073f5128b6
Jake Carpenter <jake@jakecarpenter.dev>
2026-03-30 12:28:40 -0600

    feat: use Scriban for source template
    Issue: #40

--- Optify/OptifyRegistrationSource.cs
+++ Optify/OptifyRegistrationSource.cs
@@ -1,5 +1,6 @@
 using System.Collections.Immutable;
-using System.Text;
+using Scriban;
+using Scriban.Runtime;
 
 namespace Optify;
 
@@ -7,95 +8,97 @@ public static class OptifyRegistrationSource
 {
     public const string Filename = "OptifyRegistration.g.cs";
 
+    private static readonly Template SourceTemplate = Template.Parse(TemplateString);
+
     public static string GenerateSource(ImmutableArray<OptionsTypeToRegister> optionsToRegister)
     {
-        var sb = new StringBuilder();
-        sb.AppendLine(
-            """
-            using Microsoft.Extensions.Configuration;
-            using Microsoft.Extensions.DependencyInjection;
-            using Microsoft.Extensions.Options;
-            using Microsoft.Extensions.Hosting;
+        var options = new ScriptArray();
 
-            namespace Optify;
-
-            public static partial class OptifyRegistration
-            {
-                /// <summary>
-                /// Register all configurable options types in the current assembly marked with the
-                /// <see cref="OptifyOptionsAttribute"/> attribute.
-                /// </summary>
-                /// <param name="hostBuilder">The <see cref="IHostBuilder"/> instance to extend.</param>
-                /// <returns>The extended <see cref="IHostBuilder"/> instance.</returns>
-                public static IHostBuilder UseOptify(this IHostBuilder hostBuilder)
-                {
-                    hostBuilder.ConfigureServices((ctx, services) =>
-                    {
-                        var validation = (ValidationFlag)0;
-            """
-        );
-        sb.Append(GenerateRegistrationStatements(optionsToRegister));
-        sb.AppendLine(
-            """
-                    });
-                
-                    return hostBuilder;
-                }
-            """
-        );
-
-        sb.AppendLine(
-            """
-                /// <summary>
-                /// Register all configurable options types in the current assembly marked with the
-                /// <see cref="OptifyOptionsAttribute"/> attribute.
-                /// </summary>
-                /// <param name="hostBuilder">The <see cref="IHostBuilder"/> instance to extend.</param>
-                /// <param name="validation">Specifies which validation behaviors to apply to the options registration.</param>
-                /// <returns>The extended <see cref="IHostBuilder"/> instance.</returns>
-                public static IHostBuilder UseOptify(this IHostBuilder hostBuilder, ValidationFlag validation)
-                {
-                    hostBuilder.ConfigureServices((ctx, services) =>
-                    {
-            """
-        );
-        sb.Append(GenerateRegistrationStatements(optionsToRegister));
-        sb.AppendLine(
-            """
-                    });
-                
-                    return hostBuilder;
-                }
-            }
-            """
-        );
-        return sb.ToString();
-    }
-
-    private static string GenerateRegistrationStatements(ImmutableArray<OptionsTypeToRegister> optionsToRegister)
-    {
-        var sb = new StringBuilder();
-
-        foreach (var optionsType in optionsToRegister)
+        foreach (var opt in optionsToRegister)
         {
-            sb.AppendLine("            services");
-            sb.AppendLine($"                .AddOptions<{optionsType.FullName}>()");
-            sb.AppendLine($"                .Bind(ctx.Configuration.GetSection(\"{optionsType.SectionName}\"))");
-            sb.Append($"                .MaybeAddValidation<{optionsType.FullName}>(validation)");
-
-            if (optionsType.Validation.HasFlag(ValidationFlag.DataAnnotations))
-            {
-                sb.AppendLine().Append("                .ValidateDataAnnotations()");
-            }
-
-            if (optionsType.Validation.HasFlag(ValidationFlag.OnStart))
-            {
-                sb.AppendLine().Append("                .ValidateOnStart()");
-            }
-
-            sb.AppendLine(";");
+            options.Add(
+                new ScriptObject
+                {
+                    ["full_name"] = opt.FullName,
+                    ["section_name"] = opt.SectionName,
+                    ["has_data_annotations"] = opt.Validation.HasFlag(ValidationFlag.DataAnnotations),
+                    ["has_validate_on_start"] = opt.Validation.HasFlag(ValidationFlag.OnStart),
+                }
+            );
         }
 
-        return sb.ToString();
+        var model = new ScriptObject { ["options"] = options };
+        var context = new TemplateContext();
+        context.PushGlobal(model);
+
+        return SourceTemplate.Render(context);
     }
+
+    private const string TemplateString = """
+        using Microsoft.Extensions.Configuration;
+        using Microsoft.Extensions.DependencyInjection;
+        using Microsoft.Extensions.Options;
+        using Microsoft.Extensions.Hosting;
+
+        namespace Optify;
+
+        public static partial class OptifyRegistration
+        {
+            /// <summary>
+            /// Register all configurable options types in the current assembly marked with the
+            /// <see cref="OptifyOptionsAttribute"/> attribute.
+            /// </summary>
+            /// <param name="hostBuilder">The <see cref="IHostBuilder"/> instance to extend.</param>
+            /// <returns>The extended <see cref="IHostBuilder"/> instance.</returns>
+            public static IHostBuilder UseOptify(this IHostBuilder hostBuilder)
+            {
+                hostBuilder.ConfigureServices((ctx, services) =>
+                {
+        {{~ for opt in options ~}}
+        {{~ if !for.first }}{{ "\n" }}{{ end ~}}
+                    services
+                        .AddOptions<{{ opt.full_name }}>()
+                        .Bind(ctx.Configuration.GetSection("{{ opt.section_name }}"))
+        {{- if opt.has_data_annotations }}
+                        .ValidateDataAnnotations()
+        {{- end }}
+        {{- if opt.has_validate_on_start }}
+                        .ValidateOnStart()
+        {{- end }};
+        {{~ end ~}}
+                });
+
+                return hostBuilder;
+            }
+
+            /// <summary>
+            /// Register all configurable options types in the current assembly marked with the
+            /// <see cref="OptifyOptionsAttribute"/> attribute.
+            /// </summary>
+            /// <param name="hostBuilder">The <see cref="IHostBuilder"/> instance to extend.</param>
+            /// <param name="validation">Specifies which validation behaviors to apply to the options registration.</param>
+            /// <returns>The extended <see cref="IHostBuilder"/> instance.</returns>
+            public static IHostBuilder UseOptify(this IHostBuilder hostBuilder, ValidationFlag validation)
+            {
+                hostBuilder.ConfigureServices((ctx, services) =>
+                {
+        {{~ for opt in options ~}}
+        {{~ if !for.first }}{{ "\n" }}{{ end ~}}
+                    services
+                        .AddOptions<{{ opt.full_name }}>()
+                        .Bind(ctx.Configuration.GetSection("{{ opt.section_name }}"))
+                        .MaybeAddValidation<{{ opt.full_name }}>(validation)
+        {{- if opt.has_data_annotations }}
+                        .ValidateDataAnnotations()
+        {{- end }}
+        {{- if opt.has_validate_on_start }}
+                        .ValidateOnStart()
+        {{- end }};
+        {{~ end ~}}
+                });
+
+                return hostBuilder;
+            }
+        }
+        """;
 }
